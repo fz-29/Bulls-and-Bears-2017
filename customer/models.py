47d28223decf6f22d342096d26f231510f34f0d5
SHUBHI SAREEN <shubhisareen@gmail.com>
2017-01-08 23:04:04 +0530

    Added StockHolding and StockShorted

--- customer/models.py
+++ customer/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.template.defaultfilters import truncatechars
 # Create your models here.
+
+class StockHolding(models.Model):
+    company = models.ForeignKey(stockmarket.Company, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    
+class StockShorted(models.Model):
+    company = models.ForeignKey(stockmarket.Company, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
