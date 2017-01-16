74267509f7ec17418b86157f0847999453b1c761
SHUBHI SAREEN <shubhisareen@gmail.com>
2017-01-16 19:37:06 +0530

    Correcting Models

--- customer/models.py
+++ customer/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.core.validators import MinValueValidator
 
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
@@ -22,17 +23,20 @@ class StockShorted(models.Model):
 class CustomerActivity(models.Model):
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
 	action = models.CharField(max_length=10)
-	timestamp = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
     quantity = models.PositiveIntegerField(default=0)
-    price = models.DecmalFeild(default=0,max_digits=5, decimal_places=2)
+    price = models.DecmalFeild(default=0,max_digits=9, decimal_places=2,validators=[MinValueValidator(Decimal('0.00'))])
     
     def __str__(self):
         return self.customer
 
 class Customer(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-    accont_balance = models.DecimalField(default=0,max_digits=5, decimal_places=2)
-    loan_balance = models.DecimalField(default=0,max_digits=5, decimal_places=2)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+
+    accont_balance = models.DecimalField(default=0,max_digits=15, decimal_places=2)
+    loan_balance = models.DecimalField(default=0,max_digits=15, decimal_places=2)
     stock_holdings= models.ForeignKey(StockHolding , on_delete=models.CASCADE)
     stock_shorted= models.ForeignKey(StockShorted, on_delete=models.CASCADE)
+     def __str__(self):
+        return user.first_name
     
