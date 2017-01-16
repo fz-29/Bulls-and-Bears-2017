4b7288a20e7c9b2b424ff729891f56aea44530e6
SHUBHI SAREEN <shubhisareen@gmail.com>
2017-01-16 19:39:08 +0530

    Correcting Models

--- customer/models.py
+++ customer/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
