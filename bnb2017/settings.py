b2491dbf5ffe7f936a12bdbcf7d8112b290ef34f
Feroz Ahmad <ieee@bnbmachine.localdomain>
2017-02-01 19:40:07 +0000

    colect static change in cmd

--- bnb2017/settings.py
+++ bnb2017/settings.py
@@ -145,4 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
