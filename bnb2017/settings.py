6cd9d4e155a4a58c80aa3e4e4bad8757f669221e
Feroz Ahmad <f29ahmad@gmail.com>
2017-02-02 00:55:45 +0530

    Fixed Static Path

--- bnb2017/settings.py
+++ bnb2017/settings.py
@@ -145,4 +145,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
