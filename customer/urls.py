87674f0068fc5866c0b7c0386c954328b5903420
Nikhil Kumar Mishra <nkmishra1997@gmail.com>
2017-02-03 04:23:34 +0530

    urls,views in customer(incomplete)

--- customer/urls.py
+++ customer/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     url(r'^buy/$', buyStocks, name='index'),
+    url(r'^buyinfo/$', buyinfo, name='buyinfo'),
+    url(r'^sell/$', sellStocks, name='index'),
+    url(r'^sellinfo/$', sellinfo, name='sellinfo'),
     url(r'^create/$', createCustomer, name='create'),
 ]
