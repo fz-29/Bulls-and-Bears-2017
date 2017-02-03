5ba3fd6326caa738776107a99dc1eabdf457da56
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-02-03 13:45:59 +0530

    media settings

--- bnb2017/urls.py
+++ bnb2017/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from fblogin.views import FacebookLogin 
 
@@ -25,3 +27,6 @@ urlpatterns = [
     url(r'^stockmarket/',include('stockmarket.urls')),
     url(r'^login/',include('fblogin.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
