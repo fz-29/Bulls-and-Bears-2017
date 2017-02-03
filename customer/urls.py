5382f297b3e7ab5f78f5d88eb843b747f0779a40
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-02-03 19:12:48 +0530

    url

--- customer/urls.py
+++ customer/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^customerdetail/$', customerDetail, name='customerdetail'),
     url(r'^stockholding/$', stockHolding, name='stockholding'),
     url(r'^stockshorted/$', stockShorted, name='stockshorted'),
+    url(r'^activity/$', customerActivity, name='activity'),
     # url(r'^sell/$', sellStocks, name='index'),
     # url(r'^sellinfo/$', sellinfo, name='sellinfo'),
     url(r'^create/$', createCustomer, name='create'),
