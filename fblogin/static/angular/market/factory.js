2bf179c2653d3f321a952eec1aa63165880bea15
Shivam Goel <shvmgyl15@hotmail.com>
2017-02-05 01:01:51 +0530

    loan buttons

--- fblogin/static/angular/market/factory.js
+++ fblogin/static/angular/market/factory.js
@@ -15,22 +15,7 @@ angular.module('market')
 				return response.data;
 			});
 		}
-	var customerDetail = function(authToken) {
-			return $http({
-				method: 'GET',
-				url: 'customer/customerdetail/',
-				headers: { 
-					'Authorization': authToken ,
-					'Accept': 'application/json',
-        			"X-Login-Ajax-call": 'true'
-			 }
-			}).then(function(response){
-				console.log(response);
-				return response.data;
-			});
-		}
 	return {
-		getCompanyList: companyList,
-		getAccountBalance: customerDetail
+		getCompanyList: companyList
 	}
 });
