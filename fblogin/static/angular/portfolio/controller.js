e0da660953fa5ffe5b4711590ad9d7faf3308805
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-02-05 15:48:50 +0530

    graph

--- fblogin/static/angular/portfolio/controller.js
+++ fblogin/static/angular/portfolio/controller.js
@@ -92,4 +92,20 @@ angular.module('portfolio')
 			}
 		});
 	}
+
+	var chartData = {
+		type: "line",  // Specify your chart type here.
+		title: {},
+		legend: {}, // Creates an interactive legend
+		series: [  // Insert your series data here.
+			{ values: [35, 42, 67, 89]},
+			{ values: [28, 40, 39, 36]}
+		]
+		};
+		zingchart.render({ // Render Method[3]
+		id: "chartDiv",
+		data: chartData,
+		height: 400,
+		width: 600
+    });
 });
