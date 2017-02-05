a7d02df7c47cb89e99d6a83eb5c41e2205113057
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-02-05 18:53:15 +0530

    deafult values

--- customer/views.py
+++ customer/views.py
@@ -187,8 +187,8 @@ def takeloan(request, format=None):
 	customer = get_object_or_404(Customer, user=request.user)
 	loan= get_object_or_404(Loan, customer=customer)
 	if loan.amount==0:
-		loan.amount = 10000
-		customer.account_balance += 10000 
+		loan.amount = 50000
+		customer.account_balance += 50000 
 		loan.save()
 		customer.save()
 		return JsonResponse({"success":True})
@@ -200,7 +200,7 @@ def repayloan(request, format=None):
 	customer = get_object_or_404(Customer, user=request.user)
 	if loan.amount > 0 and loan.amount <= customer.account_balance:
 		loan.amount = 0
-		customer.account_balance -= 10000
+		customer.account_balance -= 50000
 		loan.repay_time=datetime.datetime.now()
 		loan.save()
 		customer.save()
@@ -216,7 +216,7 @@ def createCustomer(request, format = None):
 	try:
 		customer = Customer.objects.get(user = user)
 	except Customer.DoesNotExist:
-		customer = Customer(user = user, account_balance = 25000)
+		customer = Customer(user = user, account_balance = 1000000)
 		companies = Company.objects.all()
 		customer.save()
 		loan = Loan(customer=customer, amount=0)
