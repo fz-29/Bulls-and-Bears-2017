bca9c3a04699298b01af9aaa8d30e599b59ed4bc
Shivam Goel <shvmgyl15@hotmail.com>
2017-02-06 19:17:12 +0530

    refresh

--- fblogin/static/angular/market/controller.js
+++ fblogin/static/angular/market/controller.js
@@ -1,14 +1,28 @@
 'use strict';
 angular.module('market')
-.controller('marketController', function($scope, $cookies, marketService) {
+.controller('marketController', function($scope, $cookies, $timeout, marketService) {
     $scope.companies = [];
 	$scope.accountBalance = 0;
     var authToken = 'Token ' + $cookies.get('authToken');
 	console.log("authToken : " + authToken);
 
-	marketService.getCompanyList(authToken).then(function(companyList){
-		$scope.accountBalance = companyList.account_balance;
-		$scope.companies = companyList.companies;
+	var refreshingPromise = null; 
+	var isRefreshing = false;
+	$scope.startRefreshing = function(){
+		if(isRefreshing) return;
+		isRefreshing = true;
+		(function refreshEvery(){
+			//Do refresh
+			//If async in then in callback do...
+			marketService.getCompanyList(authToken).then(function(companyList){
+				$scope.accountBalance = companyList.account_balance;
+				$scope.companies = companyList.companies;
+				refreshingPromise = $timeout(refreshEvery,60000)
+			});
+		}());
+	} 
+	$scope.$on('$destroy',function(){
+    if(refreshingPromise)
+        $timeout.cancel(refreshingPromise);   
 	});
-	
 });
