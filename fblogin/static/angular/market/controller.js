0863361d04c8fb9cdf58205a97c2f463a1ec4812
Shivam Goel <shvmgyl15@hotmail.com>
2017-02-06 19:08:20 +0530

    removed $interval

--- fblogin/static/angular/market/controller.js
+++ fblogin/static/angular/market/controller.js
@@ -1,68 +1,14 @@
 'use strict';
 angular.module('market')
-.controller('marketController', function($scope, $cookies, $interval, marketService) {
+.controller('marketController', function($scope, $cookies, marketService) {
     $scope.companies = [];
 	$scope.accountBalance = 0;
     var authToken = 'Token ' + $cookies.get('authToken');
 	console.log("authToken : " + authToken);
 
-	// $scope.Timer = null;
-
-	// $scope.refreshMarket = function() {
-    //    marketService.getCompanyList(authToken).then(function(companyList){
-	// 		$scope.accountBalance = companyList.account_balance;
-	// 		$scope.companies = companyList.companies;
-	// 	});
-    // }
-
-	// $scope.StartTimer = function () {
-	// 	$scope.Timer = $interval($scope.refreshMarket(), 10000);
-	// };
-
-	// $scope.StopTimer = function () {
-	// 	if (angular.isDefined($scope.Timer)) {
-	// 		$interval.cancel($scope.Timer);
-	// 	}
-	// };
-	
-	// $scope.StartTimer();
-
-	// refreshMarket();
-
-    // $scope.$on('$destroy',function(){
-    //     $scope.StopTimer();   
-    // });
-
-	var refreshingPromise = null; 
-	var isRefreshing = false;
-	$scope.startRefreshing = function(){
-		if(isRefreshing) return;
-		isRefreshing = true;
-		(function refreshEvery(){
-			//Do refresh
-			//If async in then in callback do...
-			marketService.getCompanyList(authToken).then(function(companyList){
-				$scope.accountBalance = companyList.account_balance;
-				$scope.companies = companyList.companies;
-				refreshingPromise = $timeout(refreshEvery,10000);
-			});
-		}());
-	} 
-	$scope.$on('$destroy',function(){
-    if(refreshingPromise)
-        $timeout.cancel(refreshingPromise);   
+	marketService.getCompanyList(authToken).then(function(companyList){
+		$scope.accountBalance = companyList.account_balance;
+		$scope.companies = companyList.companies;
 	});
-
-	// var intervalPromise;
-	// $scope.refreshMe = function(){
-	// 	marketService.getCompanyList(authToken).then(function(companyList){
-	// 		$scope.accountBalance = companyList.account_balance;
-	// 		$scope.companies = companyList.companies;
-	// 		intervalPromise = $interval($scope.refreshMe, 60000);
-	// 	});
-	// }
-	// $scope.$on('$destroy',function(){
-	// 	if(intervalPromise)
-	// 		$interval.cancel(intervalPromise);   
-	// });
+	
 });
