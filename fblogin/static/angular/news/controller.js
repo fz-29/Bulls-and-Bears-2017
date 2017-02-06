b2e25ac547251c760a3e360205bcd9bd6e259a3c
Shivam Goel <shvmgyl15@hotmail.com>
2017-02-06 17:29:38 +0530

    market fixes

--- fblogin/static/angular/news/controller.js
+++ fblogin/static/angular/news/controller.js
@@ -1,13 +1,11 @@
 'use strict';
 angular.module('news')
-.controller('newsController', function($scope, $cookies, $interval, newsService) {
+.controller('newsController', function($scope, $cookies, newsService) {
     $scope.newsList = [];
     var authToken = 'Token ' + $cookies.get('authToken');
 	console.log("authToken : " + authToken);
-	$scope.callAtInterval = function(){
-		newsService.getNewsList(authToken).then(function(newsList){
+	newsService.getNewsList(authToken).then(function(newsList){
 		$scope.newsList = newsList;
-	});}
+	});
 	$scope.callAtInterval();
-	// $interval( function(){ $scope.callAtInterval(); }, 60000);
 });
