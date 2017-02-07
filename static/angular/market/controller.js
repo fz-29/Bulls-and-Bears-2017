552ce4faa9f1ab29b1d75fb3c61c5ec5086e1d57
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-02-07 17:36:03 +0530

    graph and portfolio

--- static/angular/market/controller.js
+++ static/angular/market/controller.js
@@ -1,15 +1,29 @@
 'use strict';
 angular.module('market')
-.controller('marketController', function($scope, $cookies, $interval, marketService) {
+.controller('marketController', function($scope, $cookies, $timeout, marketService) {
     $scope.companies = [];
 	$scope.accountBalance = 0;
     var authToken = 'Token ' + $cookies.get('authToken');
 	console.log("authToken : " + authToken);
-	$scope.callAtInterval = function(){
-		marketService.getCompanyList(authToken).then(function(companyList){
-		$scope.accountBalance = companyList.account_balance;
-		$scope.companies = companyList.companies;
-	});}
-	$scope.callAtInterval();
-	$interval( function(){ $scope.callAtInterval(); }, 60000);
+
+	var refreshingPromise = null; 
+	var isRefreshing = false;
+	$scope.startRefreshing = function(){
+		if(isRefreshing) return;
+		isRefreshing = true;
+		(function refreshEvery(){
+			//Do refresh
+			//If async in then in callback do...
+			marketService.getCompanyList(authToken).then(function(companyList){
+				$scope.accountBalance = companyList.account_balance;
+				$scope.companies = companyList.companies;
+				refreshingPromise = $timeout(refreshEvery,60000);
+			});
+		}());
+	} 
+	$scope.startRefreshing();
+	$scope.$on('$destroy',function(){
+    if(refreshingPromise)
+        $timeout.cancel(refreshingPromise);   
+	});
 });
--- static/angular/news/controller.js
+++ static/angular/news/controller.js
@@ -1,17 +1,14 @@
 'use strict';
 angular.module('news')
-.controller('newsController', function($scope, $sce, $cookies, $interval, newsService) {
+.controller('newsController', function($scope, $sce, $cookies, newsService) {
     $scope.newsList = [];
     var authToken = 'Token ' + $cookies.get('authToken');
 	console.log("authToken : " + authToken);
-	$scope.callAtInterval = function(){
-		newsService.getNewsList(authToken).then(function(newsList){
+	newsService.getNewsList(authToken).then(function(newsList){
 		$scope.newsList = newsList;
 		for (var news in $scope.newsList) {
 			$scope.newsList[news].fields.news_text = $sce.trustAsHtml($scope.newsList[news].fields.news_text);
 			$scope.newsList[news].fields.youtube_src = $sce.trustAsResourceUrl($scope.newsList[news].fields.youtube_src);
  		}
-	});}
-	$scope.callAtInterval();
-	$interval( function(){ $scope.callAtInterval(); }, 60000);
+	});
 });
